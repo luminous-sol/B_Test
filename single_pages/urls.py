a35f39d4d7cdac6d71198408a282e54d53105fef
coding.soli <coding.soli@gmail.com>
2022-12-11 04:22:57 +0000

    Blog Main Page

---
+++ single_pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . imoort views 
+
+urlpatterns = [
+    path('about_me/', views.about_me) 
+    # about_me라는 path를 urls을 얻어올거다. views 에서
+    path('', views.landing)
+    # views에서 about_me 와 landing 을 받아 올 것이다. 
+    # 현재 디렉토리의 views에서 import 하라고 하였으므로 views에 가본다. 
+    # 하지만 views에는 아무것도 존재하지 않기 때문에 다음과 같이 입력해준다. 
+]
--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+def landing(request):
+    return render(
+    request,
+    'single_pages/landing.html'
+    )
+
+def about_me(request):
+    return render(
+    request
+    'single_pages/about_me.html'
+    )
+# single_pages 에서 받아 올 landing.html, about_me.html 파일을 만들어준다. 
+# landing.html 이 메인 그리고 about_me.html 이 내부 페이지가 될 예정이다. 
