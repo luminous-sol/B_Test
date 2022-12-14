ed1f694d874ad8db439b639323ff5e4ba15e1fe8
coding.soli <coding.soli@gmail.com>
2022-12-14 08:30:05 +0000

    Blog

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-
+import os
 # def (힘수)사용해서 작성하는 방법
 
 # class 사용해서 작성하는 방법
 class Post(models.Model):
     title = models.CharField(max_length = 70)
     # 제목 최대 길이
+    hook_text = models.CharField(max_length = 100, blank = True)
+    # admin에서 hook_text를 넣을 수 있는 공간을 만들어 주자
     content = models.TextField()
     # 콘텐츠는 텍스트필드불러온다(글적는 칸)
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
@@ -20,8 +22,17 @@ class Post(models.Model):
     
     def __str__(self) :
         return f'[{self.pk}]{self.title}'
-        # Post object(1) 이렇게 표시되는 콘텐츠를 제목이 나오도록 변경시켜준다. 
+        # Post object(1) 이렇게 표시되는 콘텐츠를 제목이 나오도록 변경시켜준다.
+        
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
-# Create your models here.
+    
+    # 경로 제외한 파일명 받아오기
+    def get_file_name(self): 
+        return os.path.basename(self.file_upload.name)
+    
+    # 확장자 받아오기 / ext = extension 확장자
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
+    
 # models.py 수정 후에는 무조건 migration 해주어야 한다. 
