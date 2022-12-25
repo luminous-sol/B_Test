64fbd2efb86750872905582019ed77fab21b461b
coding.soli <coding.soli@gmail.com>
2022-12-25 13:50:00 +0000

    soli's blog

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def landing(request):
+    recent_posts = Post.objects.order_by('-pk')[:3]
+    # 끝에서 세 개(역순으로) 최근 게시글가지고 오기
+
     return render(
     request,
-    'single_pages/landing.html'
+    'single_pages/landing.html',{
+        'recent_posts' : recent_posts,
+    }
     )
 
 def about_me(request):
